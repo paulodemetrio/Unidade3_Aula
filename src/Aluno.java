11009d66688d674c94de355ed4bc78a10402456a
paulodemetrio <pdemetriocom@gmail.com>
2025-03-05 19:23:25 -0300

    t

---
+++ src/Aluno.java
@@ -0,0 +1,10 @@
+public class Aluno {
+    public int nMatricula;
+    public String nome;
+    public char sexo;
+
+    public int informaMatricula(int nMatricula) {
+        this.nMatricula = nMatricula;
+        return nMatricula;
+    }
+}
---
+++ src/Car.java
@@ -0,0 +1,45 @@
+public class Car {
+    // Fields
+    private String color;
+    private String model;
+    private int year;
+
+    // Constructor
+    public Car(String color, String model, int year) {
+        this.color = color;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Method
+    public void displayDetails() {
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Color: " + color);
+        System.out.println("Car Year: " + year);
+    }
+
+    // Getter and Setter methods
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
